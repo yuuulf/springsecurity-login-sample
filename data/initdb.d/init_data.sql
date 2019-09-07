b5fa4f5b43cab537b855f96af7ba44dbf7eb41a8
Yui Tatano <smharb525@gmail.com>
2019-09-07 23:46:15 +0900

    mysql構築のdocker-compose.yml、データを追加

---
+++ data/initdb.d/init_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `sample`.`users` (`mail_address`, `password`, `user_name`, `valid_flag`, `update_user`, `update_date`) VALUES
+('test@aaa.co.jp', '$2a$10$dxAHfwD35ncDiByAmhjnVe1puBSzAKu9GoSgbhalCb49s4BiIp2Mi', 'test user', 1, 1, now()),
+('test2@aaa.co.jp', '$2a$10$LpPYN.acXh02I.KWZwVTpugTfeOkfByoL8B8Bp4fFEttfngCF47d6', 'test user2', 1, 1, now()),
+('test3@aaa.co.jp', '$2a$10$dxAHfwD35ncDiByAmhjnVe1puBSzAKu9GoSgbhalCb49s4BiIp2Mi', 'test user3', 0, 1, now());
